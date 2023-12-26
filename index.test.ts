9408a7361ee0d4c4ad9997ae3e19236f87bc8701
dkoldaev <dkoldyaev@gmai.com>
2023-12-26 17:10:38 +0400

    feature-1: test for case two equal words

--- index.test.ts
+++ index.test.ts
@@ -10,4 +10,14 @@ describe('Command line execution', () => {
 
     expect(output).toBe(expectedOutput);
   });
+
+  it('should correctly execute from command line if words are the same', () => {
+    const word1 = 'sitting';
+    const word2 = 'sitting';
+    const expectedOutput = '0\nsitting\n';
+
+    const output = execSync(`ts-node index.ts ${word1} ${word2}`, { encoding: 'utf8' });
+
+    expect(output).toBe(expectedOutput);
+  });
 });
