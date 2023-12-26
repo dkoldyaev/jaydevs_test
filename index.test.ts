9b1b143bbb9b0b6240a9aaa96c18885718970b38
dkoldaev <dkoldyaev@gmai.com>
2023-12-26 16:34:01 +0400

    feature-1: positive test for console running

--- index.test.ts
+++ index.test.ts
@@ -1,1 +1,13 @@
-describe('updatePage', () => {});
+import { execSync } from 'child_process';
+
+describe('Command line execution', () => {
+  it('should correctly execute from command line', () => {
+    const word1 = 'kitten';
+    const word2 = 'sitting';
+    const expectedOutput = '3\nsitting\nsittin\nsitten\nkitten\n';
+
+    const output = execSync(`ts-node index.ts ${word1} ${word2}`, { encoding: 'utf8' });
+
+    expect(output).toBe(expectedOutput);
+  });
+});
