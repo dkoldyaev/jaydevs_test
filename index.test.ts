563019b018e8d72302d76d7b21c9623301b36b32
dkoldaev <dkoldyaev@gmai.com>
2023-12-28 16:51:06 +0400

    10-fix: test coverage

--- index.test.ts
+++ index.test.ts
@@ -1,6 +1,12 @@
 import { execSync } from 'child_process';
+import { insertIntoArray, minimalDistance, showError } from './index';
+
 
 describe('Command line execution', () => {
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
   it('should correctly execute from command line', () => {
     const word1 = 'kitten';
     const word2 = 'sitting';
@@ -47,5 +53,101 @@ describe('Command line execution', () => {
     const output = execSync(`ts-node index.ts ${word1} ${word2} ${word3}`, { encoding: 'utf8' });
 
     expect(output).toBe(expectedError);
+  });
+});
+
+describe('insertIntoArray', () => {
+
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
+  it('must insert correct', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const toIndex = 2;
+    const insertValue = 999;
+
+    const result = insertIntoArray(arr, toIndex, insertValue);
+    expect(result).toEqual([1, 2, 999, 3, 4, 5]);
+  });
+
+  it('must insert to first', () => {
+    const arr = [1, 2, 3, 4, 5];
+    const toIndex = 0;
+    const insertValue = 999;
+
+    const result = insertIntoArray(arr, toIndex, insertValue);
+    expect(result).toEqual([999, 1, 2, 3, 4, 5]);
+  });
+});
+
+describe('minimalDistance', () => {
+  const {
+    log: baseConsoleLog,
+    error: baseConsoleErr,
+    warn: baseConsoleWar,
+  } = console;
+
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
+  beforeEach(() => {
+    console.log = jest.fn();
+    console.error = jest.fn();
+    console.warn = jest.fn();
+  });
+
+  afterEach(() => {
+    console.log = baseConsoleLog;
+    console.error = baseConsoleErr;
+    console.warn = baseConsoleWar;
+  });
+
+  it('works correct with only replace', () => {
+    const word1 = 'word1';
+    const word2 = 'port2';
+
+    const result = minimalDistance(word1, word2);
+
+    expect(result).not.toBeDefined();
+    expect(console.error).not.toHaveBeenCalled();
+    expect(console.warn).not.toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledTimes(5);
+    expect(console.log).toHaveBeenCalledWith(3);
+    expect(console.log).toHaveBeenCalledWith('port2');
+    expect(console.log).toHaveBeenCalledWith('port1');
+    expect(console.log).toHaveBeenCalledWith('pord1')
+    expect(console.log).toHaveBeenCalledWith('word1');
+  });
+
+  it('works correct with delete', () => {
+    const word1 = 'aaa';
+    const word2 = 'abaa';
+
+    const result = minimalDistance(word1, word2);
+
+    expect(result).not.toBeDefined();
+    expect(console.error).not.toHaveBeenCalled();
+    expect(console.warn).not.toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledTimes(3);
+    expect(console.log).toHaveBeenCalledWith(1);
+    expect(console.log).toHaveBeenCalledWith('abaa');
+    expect(console.log).toHaveBeenCalledWith('aaa');
+  });
+
+  it('works correct with insert', () => {
+    const word1 = 'abaa';
+    const word2 = 'aaa';
+
+    const result = minimalDistance(word1, word2);
+
+    expect(result).not.toBeDefined();
+    expect(console.error).not.toHaveBeenCalled();
+    expect(console.warn).not.toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledTimes(3);
+    expect(console.log).toHaveBeenCalledWith(1);
+    expect(console.log).toHaveBeenCalledWith('aaa');
+    expect(console.log).toHaveBeenCalledWith('abaa');
   });
 });
