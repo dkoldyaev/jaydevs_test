59146ff3502684864aae75f55fe7d3f6a22edc56
dkoldaev <dkoldyaev@gmai.com>
2024-01-05 03:50:45 +0400

    7: reformat command structure

--- src/enums.ts
+++ src/enums.ts
@@ -1,5 +1,6 @@
 export enum ErrorMessagesEnum {
   WRONG_PARAMS_COUNT = 'Wrong arguments. Use mpm run start --help to check syntax',
+  WRONG_COMMAND = 'Wrong command',
 }
 
 export enum DefaultChangePrice {
